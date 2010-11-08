9922bdee42ee13af5defb5279aebd04f989f6e05
jessebmiller <jessebmiller@gmail.com>
2010-11-08 08:35:41 -0800

    renamed some verriables

--- wbip.py
+++ wbip.py
@@ -8,14 +8,14 @@ from xml.dom import minidom
 #cityOf should return the zip code associated with the IP address it is passed. 
 def locOf( ip ):
   apiCall = "http://api.hostip.info/?ip=" + ip
-  connection = urllib.urlopen(apiCall)
-  loc = minidom.parse(connection)
+  apiResponse = urllib.urlopen(apiCall)
+  loc = minidom.parse(apiResponse)
   return loc
 
 def weatherOf( loc ):
   apiCall = "http://www.google.com/ig/api/?weather=" + loc
-  connection = urllib.urlopen(apiCall)
-  weather = connection.read()
+  apiResponse = urllib.urlopen(apiCall)
+  weather = minidom.parse(apiResponse)
   return weather
 
 ip = raw_input("input IP to look up: ")
