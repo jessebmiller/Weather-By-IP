3da314500757e33f4b9fdf19509b9499c1c51f5b
jessebmiller <jessebmiller@gmail.com>
2010-11-07 22:10:39 -0800

    started to add xml parsing

--- wbip.py
+++ wbip.py
@@ -3,12 +3,22 @@
 #Weather by IP address must recieve an IP address as an arguement, gather the zip code for that IP, then gather and display the weather for that zip code.
 
 import urllib
+from xml.dom import minidom
 
 #cityOf should return the zip code associated with the IP address it is passed. 
 def locOf( ip ):
   apiCall = "http://api.hostip.info/?ip=" + ip
   connection = urllib.urlopen(apiCall)
-  loc = connection.read()
-  print loc
+  loc = minidom.parse(connection)
+  return loc
 
-locOf( raw_input("input IP to look up: ") )
+def weatherOf( loc ):
+  apiCall = "http://www.google.com/ig/api/?weather=" + loc
+  connection = urllib.urlopen(apiCall)
+  weather = connection.read()
+  return weather
+
+ip = raw_input("input IP to look up: ")
+loc = locOf( ip )
+
+print loc
