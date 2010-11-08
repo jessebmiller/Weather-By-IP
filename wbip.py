4c658b7c35ed4d7c5d8ef44063f5452b527ba261
jessebmiller <jessebmiller@gmail.com>
2010-11-07 22:15:58 -0800

    fixed comments

--- wbip.py
+++ wbip.py
@@ -5,13 +5,15 @@
 import urllib
 from xml.dom import minidom
 
-#cityOf should return the zip code associated with the IP address it is passed. 
+#should return the location associated with the IP address it is passed.
+#loc should be in a form that google ig weather api can understand. (city or zip) 
 def locOf( ip ):
   apiCall = "http://api.hostip.info/?ip=" + ip
   connection = urllib.urlopen(apiCall)
   loc = minidom.parse(connection)
   return loc
 
+#should return weather information for the location given as an XML dom object.
 def weatherOf( loc ):
   apiCall = "http://www.google.com/ig/api/?weather=" + loc
   connection = urllib.urlopen(apiCall)
