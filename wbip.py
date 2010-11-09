c2f2bcca8fdcb8761a33c2fd777b425ee114385a
jessebmiller <jessebmiller@gmail.com>
2010-11-08 22:29:09 -0800

    locOf( ip ) works, working on weatherOf ( loc ) added tests: should ad more, learn more about writing good python tests

--- wbip.py
+++ wbip.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -tt
 
 #Weather by IP address must recieve an IP address as an arguement, gather the zip code for that IP, then gather and display the weather for that zip code.
 
@@ -10,17 +10,34 @@ from xml.dom import minidom
 def locOf( ip ):
   apiCall = "http://api.hostip.info/?ip=" + ip
   apiResponse = urllib.urlopen(apiCall)
-  loc = minidom.parse(apiResponse)
+  hostip = minidom.parse(apiResponse)
+  hostipNames = hostip.getElementsByTagName("gml:name")
+  loc = hostipNames[1].firstChild.data
   return loc
 
 #should return weather information for the location given as an XML dom object.
 def weatherOf( loc ):
-  apiCall = "http://www.google.com/ig/api/?weather=" + loc
+  apiCall = "http://www.google.com/ig/api?weather=" + loc
   apiResponse = urllib.urlopen(apiCall)
-  weather = minidom.parse(apiResponse)
+  weatherDom = minidom.parse(apiResponse)
+  currentCondition = weatherDom.getElementsByTagName('condition')
+  weather = currentCondition.firstChild.data
   return weather
 
-ip = raw_input("input IP to look up: ")
+ip = "8.8.8.8"
 loc = locOf( ip )
+weather = weatherOf( loc )
 
-print loc
+print "\n\n" + loc, weather
+
+#tests
+print "\n\nTests:"
+
+#should return Mounain View, CA
+if locOf("8.8.8.8") == "Mountain View, CA":
+  print "locOf( ip ) PASS"
+else:
+  print "locOf( ip ) FAIL"
+
+#add white space to give results some room
+print "\n\n"
