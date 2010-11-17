57c29d74a591d31207602fead500a3d5d80ebc6f
jessebmiller <jessebmiller@gmail.com>
2010-11-16 21:19:11 -0800

    Added some primitave ip address validation

--- wbip.py
+++ wbip.py
@@ -2,11 +2,12 @@
 
 #Weather by IP address must recieve an IP address as an arguement, gather the zip code for that IP, then gather and display the weather for that zip code.
 
+import re
 import sys
 import urllib
 from xml.dom import minidom
 
-#should return the location associated with the IP address it is passed.
+#returns the location associated with the IP address it is passed.
 #loc should be in a form that google ig weather api can understand. (city or zip) 
 def locOf( ip ):
   apiCall = "http://api.hostip.info/?ip=" + ip
@@ -16,7 +17,7 @@ def locOf( ip ):
   loc = hostipNames[1].firstChild.data
   return loc
 
-#should return weather information for the location given as an XML dom object.
+#returns weather information according to the google ig weather api for the location given as an XML dom object.
 def weatherOf( loc ):
   apiCall = "http://www.google.com/ig/api?weather=" + loc
   apiResponse = urllib.urlopen(apiCall)
@@ -28,10 +29,15 @@ def main():
   if len(sys.argv) != 2:
     print 'usage: python wbip.py ip-to-check'
     sys.exit(1)
-
-  ip = sys.argv[1]
-  print "the current weather conditions at IP:", ip, "are", weatherOf( locOf(ip) )+'.'
-
+    
+  #verify that the arguement is in fact an IP address
+  print sys.argv[1]
+  match = re.search( '\d+\.\d+\.\d+\.\d+' , sys.argv[1])   #pull out of the passed arguement an IP like pattern
+  if match:                #if that pattern is the passed arguement, assume it is an ip address
+    ip = match.group()
+    print 'the current weather conditions at IP:', ip, 'are', weatherOf( locOf(ip) )+'.'
+  else:
+    print 'First arguement must contain the IP address to look up';
   
 if __name__ == '__main__':
   main()
